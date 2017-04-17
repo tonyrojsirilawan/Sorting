a38d5474ecdcd03c31782507b7be41db67489b57
tonyrojsirilawan <trojsirilawan@albany.edu>
2017-04-16 20:34:49 -0400

    Completed

--- Sorting/src/sorting/BubbleSort.java
+++ Sorting/src/sorting/BubbleSort.java
@@ -2,7 +2,7 @@ package sorting;
 
 public class BubbleSort {
 
-	public BubbleSort(int[] list){
+	static void bubbleSort(int[] list){
 		int n = list.length;
 		int temp = 0;
 		for(int i=0; i < n; i++){
@@ -18,7 +18,18 @@ public class BubbleSort {
 	public static void main (String args[]){
 		int list[] = {5, 48, 8, 46, 2, 24, 52, 12};
 		System.out.println("List before Bubble Sort");
-			
+		
+		for(int i=0; i < list.length; i++){
+			System.out.print(list[i] + " ");
+		}
+		System.out.println();
+		
+		bubbleSort(list);
+		
+		System.out.println("List after Bubble Sort");
+		for(int i=0; i < list.length; i++){
+			System.out.print(list[i] + " ");
+		}
 	}
 	
 }
