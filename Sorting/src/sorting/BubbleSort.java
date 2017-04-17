98273edef2c7ede3f8669146c6d3680ebd0edf7b
tonyrojsirilawan <trojsirilawan@albany.edu>
2017-04-16 20:27:23 -0400

    Method complete

--- Sorting/src/sorting/BubbleSort.java
+++ Sorting/src/sorting/BubbleSort.java
@@ -2,8 +2,23 @@ package sorting;
 
 public class BubbleSort {
 
+	public BubbleSort(int[] list){
+		int n = list.length;
+		int temp = 0;
+		for(int i=0; i < n; i++){
+			for(int j=1; j < (n-i); j++){
+				if(list[j-1] > list[j]){
+					temp = list[j-1];
+					list[j-1] = list[j];
+					list[j] = temp;
+				}
+			}
+		}
+	}
 	public static void main (String args[]){
-		System.out.println("aCool");
+		int list[] = {5, 48, 8, 46, 2, 24, 52, 12};
+		System.out.println("List before Bubble Sort");
+			
 	}
 	
 }
